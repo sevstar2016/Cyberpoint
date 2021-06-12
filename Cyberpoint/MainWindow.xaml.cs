3d68568d29601253d4643af8e3c25ef659e1b452
sevstar2016 <sevstar2016@gmail.com>
2021-06-12 15:00:28 +0300

    Win Lock

--- Cyberpoint/MainWindow.xaml.cs
+++ Cyberpoint/MainWindow.xaml.cs
@@ -1,5 +1,4 @@
-﻿using Cyberpoint.ViewModels;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,6 +12,12 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Diagnostics;
+using System.Threading;
+using System.ComponentModel;
+using Prism.Common;
+using Cyberpoint.ViewModels;
+using Microsoft.Win32;
 using Cyberpoint.commands;
 
 namespace Cyberpoint
@@ -22,11 +27,59 @@ namespace Cyberpoint
     /// </summary>
     public partial class MainWindow : Window
     {
+        static bool close = false;
+
         public MainWindow()
         {
             InitializeComponent();
 
-            DataContext = new MainViewModel();
+            DataContext = new RegVM();
+
+            Thread thr = new Thread(new ThreadStart(KillProcess));
+            thr.Priority = ThreadPriority.AboveNormal;
+            thr.IsBackground = false;
+            thr.Start();
+
+            Client cl = new Client();
+
+            cl.StartClient();
+        }
+
+        static void KillProcess()
+        {
+            do
+            {
+                Process[] processInfo = Process.GetProcessesByName("explorer");
+                if (processInfo != null)
+                {
+                    try
+                    {
+                        foreach (Process p in processInfo)
+                            p.Kill();
+                    }
+                    catch (Exception) { }
+                }
+            }
+            while (!close);
+        }
+
+        void Close(object sender, CancelEventArgs e)
+        {
+            //e.Cancel = true;
+            close = true;
+            Process.Start("explorer.exe");
+            //SetTaskManager(false);
+        }
+
+        public void SetTaskManager(bool enable)
+        {
+            RegistryKey objRegistryKey = Registry.CurrentUser.CreateSubKey(
+                @"Software\Microsoft\Windows\CurrentVersion\Policies\System");
+            if (enable && objRegistryKey.GetValue("DisableTaskMgr") != null)
+                objRegistryKey.DeleteValue("DisableTaskMgr");
+            else
+                objRegistryKey.SetValue("DisableTaskMgr", "1");
+            objRegistryKey.Close();
         }
     }
 }
--- Cyberpoint/ViewModels/BronVM.cs
+++ Cyberpoint/ViewModels/BronVM.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Cyberpoint.ViewModels
 {
-    public class GamesViewModel : BaseViewModel
+    class BronVM
     {
     }
 }
